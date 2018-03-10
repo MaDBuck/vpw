4f46ecb153e0961bc322ca8a67e9858199d63d48
Robin De Baets <robindebaetsdev@gmail.com>
2018-03-10 20:07:45 +0100

    Added iteration

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 # alphabet
 
 import string
+
 string.ascii_lowercase
 string.ascii_uppercase
 
@@ -29,12 +30,14 @@ b = list(a)
 # Object kopieren
 
 import copy
+
 old_list = [[1, 2], [3, 4]]
 new_list = copy.deepcopy(old_list)
 
 # case insensitive regex search
 
 import re
+
 ignorecase = re.compile("test", re.IGNORECASE)
 re.match("test", "TesT", re.IGNORECASE)
 
@@ -45,7 +48,6 @@ import numpy
 array = [[0, 1], [1, 0]]
 print(numpy.matrix(array))
 
-
 # transpose array
 
 import numpy
@@ -53,20 +55,24 @@ import numpy
 array = [[0, 1], [1, 0]]
 print(numpy.transpose(array))
 
-
 # cache method
 
 from functools import lru_cache as cache
+
 
 @cache(maxsize=None)
 def test():
     pass
 
+
 # alle permutaties van lijst
 
 import itertools
+
 list(itertools.permutations([1, 2, 3]))
 
+# alle combinaties van meerdere lijsten
 
-
-
+import itertools
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9, 10]]
+list(itertools.product(*a))
