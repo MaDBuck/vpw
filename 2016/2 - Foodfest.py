7683d21a45458edb04f7e6ab4f4b2589b828a3a4
Robin De Baets <robindebaetsdev@gmail.com>
2018-03-11 00:00:47 +0100

    Oefening opgelost

--- 2016/2 - Foodfest.py
+++ 2016/2 - Foodfest.py
@@ -2,13 +2,20 @@ import itertools
 
 
 def count():
-    budgets = input().split()[1:]
-    trucks = (((int(x) for x in input().split()[1:]) for _ in range(int(input()))))
+    budgets = list(int(x) for x in input().split()[1:])
+    trucks = list(list(int(x) for x in input().split()[1:]) for _ in range(int(input())))
+    minV = sum(min(t) for t in trucks)
+    maxV = max(budgets)
+    budgets = list(filter(lambda x: minV <= x, budgets))
+    if len(budgets) == 0:
+        return "GEEN"
+    for t in trucks:
+        t = set(filter(lambda x: minV - min(t) + x <= maxV, t))
     ans = []
     m = set(map(sum, itertools.product(*trucks)))
     for b in budgets:
-        if int(b) in m:
-            ans.append(b)
+        if b in m:
+            ans.append(str(b))
     return " ".join(ans) if len(ans) > 0 else "GEEN"
 
 
