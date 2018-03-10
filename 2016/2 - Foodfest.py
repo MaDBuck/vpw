d03e4a0c729b2c59bd395dc5392b2c381f6a0147
Robin De Baets <robindebaetsdev@gmail.com>
2018-03-10 23:31:20 +0100

    Oefening opgelost

--- 2016/2 - Foodfest.py
+++ 2016/2 - Foodfest.py
@@ -1,41 +1,16 @@
 import itertools
 
-#Verbeter snelheid
 
 def count():
-    budgets = input().split()
-    budgets.pop(0)
-
-    n = int(input())
-
-    trucks = [' '] * n
-
-    for t in range(n):
-            trucks[t] = input().split()
-            trucks[t].pop(0)
-
-    for t in range(len(trucks)):
-        for x in range(len(trucks[t])):
-            trucks[t][x] = int(trucks[t][x])
-
-    combs = list(itertools.product(*trucks))
-
+    budgets = input().split()[1:]
+    trucks = (((int(x) for x in input().split()[1:]) for _ in range(int(input()))))
     ans = []
-
-    som = 0
-
+    m = set(map(sum, itertools.product(*trucks)))
     for b in budgets:
-        for l in map(sum, combs):
-            if l == int(b):
-                ans.append(l)
-
-    st = ""
-
-    for i in set(ans):
-        st += str(i) + " "
-
-    return st if len(ans) > 0 else "GEEN"
+        if int(b) in m:
+            ans.append(b)
+    return " ".join(ans) if len(ans) > 0 else "GEEN"
 
 
-for i in range(1, int(input()) + 1):
-    print("{} {}".format(i, count().strip()))
+for i in range(int(input())):
+    print("{} {}".format(i + 1, count()))
